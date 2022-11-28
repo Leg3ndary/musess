11db1559802f00e2f9e698e59e19e2506980036c
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-11-27 23:02:45 -0500

    I don't know what I'm doing

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// npx babel --watch src --out-dir . --presets react-app/prod
+
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  )
+}
